b749a0c6bbac184adacf8cacac2f321c136041ea
tkgshn <takagishunsuke1129@gmail.com>
2025-05-01 18:27:49 +0900

    chore: Supabaseの環境変数をデバッグ出力

--- frontend/utils/supabase/client.ts
+++ frontend/utils/supabase/client.ts
@@ -1,6 +1,9 @@
 import { createBrowserClient } from "@supabase/ssr";
 import { Database } from "@/types/database.types";
 
+console.log('SUPABASE_URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log('SUPABASE_ANON_KEY:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
 export const createClient = () =>
 	createBrowserClient<Database>(
 		process.env.NEXT_PUBLIC_SUPABASE_URL!,
